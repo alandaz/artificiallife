9edf56ddab7328660b252b006081ae8ca6ed16e7
alandaz <alandazong@gmail.com>
2023-01-09 13:37:40 -0600

    robot

--- generate.py
+++ generate.py
@@ -1,11 +1,20 @@
 
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
 
-pyrosim.Send_Cube(name = "Box", pos = [0,0,0.5], size = [1,1,1])
-pyrosim.End() 
+def Create_World(): 
+    #pyrosim.Send_Cube(name = "Box", pos = [0,0,0.5], size = [1,1,1])
+    pyrosim.End() 
 
+def Create_Robot(): 
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, 0.5] , size=[1,1,1])
+    pyrosim.Send_Joint(name = "FrontLeg_Torso" , parent= "FrontLeg" , child = "Torso" , type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name="Torso", pos=[0.5, 0, 0.5] , size=[1,1,1])
+    pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1, 0, 0])
+    pyrosim.Send_Cube(name="BackLeg", pos=[0.5, 0, -.5] , size=[1,1,1])
+    pyrosim.End()
 
+pyrosim.Start_SDF("world.sdf")
 Create_World() 
 Create_Robot() 
